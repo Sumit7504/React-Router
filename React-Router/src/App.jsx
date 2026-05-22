c92c17cd73575defb7aa6bb7fa8df00766a8b982
Sumit <sumitsahu7504@gmail.com>
2026-05-22 12:10:54 +0530

    add App.jsx

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,17 @@
+// import BasicRouting from "./React-Router/basicRouting.jsx";
+// import Appuseparams from "./React-Router/useparams.jsx";
+// import AppOutlet from "./React-Router/NestedRoutes&Layouts.jsx";
+// import NavLinkApp from "./React-Router/NavLink.jsx";
+import VanList from "./React-Router/useSearchParams.jsx";
+export default function App() {
+  return (
+    <div>
+      <h1>React Router</h1>
+      {/* <BasicRouting />
+      <Appuseparams /> */}
+      {/* <AppOutlet /> */}
+      {/* <NavLinkApp /> */}
+      <VanList />
+    </div>
+  );
+}
